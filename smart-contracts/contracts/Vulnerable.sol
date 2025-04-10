2ca8f8d050171dc872b4d166e59f3e70d9f1a7d0
sophi <sel5881@g.rit.edu>
2025-04-10 13:07:41 -0400

    Added a withdraw() function in Vulnerable.sol

--- smart-contracts/contracts/Vulnerable.sol
+++ smart-contracts/contracts/Vulnerable.sol
@@ -7,4 +7,13 @@ contract Vulnerable {
     function donate() external payable {
         balances[msg.sender] += msg.value;
     }
+
+    function withdraw(uint256 amount) public {
+        require(balances[msg.sender] >= amount, "Not enough balance");
+
+        // Vulnerable line: seting ETH before updating balance
+        (bool success, ) = msg.sender.call{value: amount}("");
+        require(success, "Failed to send Ether");
+        balances[msg.sender] -= amount;
+    }
 }
